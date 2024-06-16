2bdd37fe97453dd698062d038a05e2d38256d61f
DrobnDan <drobnicdanny@gmail.com>
2024-06-16 15:07:06 -0500

    Created Basic Spawn Logic
    Added a Spawn Manager with a script to create random animals at random positions.

---
+++ Assets/Scripts/Spawn Manager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] spawnAnimals;
+    private int spawnHeight = 15;
+    private float spawnX;
+    private float spawnZ;
+    private int spawnDelay = 5;
+    private Quaternion spawnRotation = new Quaternion(0, 0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnAnimal", 0, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 GenerateSpawnPosition()
+    {
+        spawnZ = Random.Range(-8, 12);
+        spawnX = Random.Range(-12, 7);
+        Vector3 spawnPos = new Vector3(spawnX, spawnHeight, spawnZ);
+        return spawnPos;
+    }
+
+    public GameObject GenerateSpawnAnimal()
+    {
+       GameObject spawnAnimal = spawnAnimals[Random.Range(0, spawnAnimals.Length)];       
+        return spawnAnimal;
+    }
+
+    public void SpawnAnimal()
+    {
+        Instantiate(GenerateSpawnAnimal(), GenerateSpawnPosition(), spawnRotation);
+    }
+}
